13fbecb77dcc3da04473a4aa7c3b7a248d8b5663
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2021-03-26 16:11:55 -0300

    [KOGITO-3507] Adding custom TrustStore secret attribute to KogitoRuntime (#11)
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,10 @@
+The RHPAM Kogito Operator is based on Kogito Operator. 
+Please, see the upstream [RELEASE NOTES](https://github.com/kiegroup/kogito-operator/blob/master/RELEASE_NOTES.md).
+
+<!-- Remove the comment for any RHPAM JIRA exclusive 
 ## Enhancements  
 
 ## Bug Fixes
 
 ## Known Issues
+-->
--- api/v1/kogitoservices.go
+++ api/v1/kogitoservices.go
@@ -235,6 +235,13 @@ type KogitoServiceSpec struct {
 	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=false
 	// +optional
 	Probes KogitoProbe `json:"probes,omitempty"`
+
+	// Custom JKS TrustStore that will be used by this service to make calls to TLS endpoints.
+	// It's expected that the secret has two keys: `keyStorePassword` containing the password for the KeyStore
+	// and `cacerts` containing the binary data of the given KeyStore.
+	// +operator-sdk:gen-csv:customresourcedefinitions.specDescriptors=false
+	// +optional
+	TrustStoreSecret string `json:"trustStoreSecret,omitempty"`
 }
 
 // GetReplicas ...
@@ -379,4 +386,14 @@ func (k *KogitoServiceSpec) SetProbes(probes api.KogitoProbeInterface) {
 	if newProbes, ok := probes.(*KogitoProbe); ok {
 		k.Probes = *newProbes
 	}
+}
+
+// GetTrustStoreSecret ...
+func (k *KogitoServiceSpec) GetTrustStoreSecret() string {
+	return k.TrustStoreSecret
+}
+
+// SetTrustStoreSecret ...
+func (k *KogitoServiceSpec) SetTrustStoreSecret(trustStoreSecret string) {
+	k.TrustStoreSecret = trustStoreSecret
 }
