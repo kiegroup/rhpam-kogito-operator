f38c4727fe836e11670bae72b74c2e89602ae171
Karel Suta <ksuta@redhat.com>
2021-04-20 14:51:05 +0200

    [KOGITO-4846] Create a nightly job to update go.mod file and run BDD tests
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- .jenkins/dsl/jobs.groovy
+++ .jenkins/dsl/jobs.groovy
@@ -22,12 +22,29 @@ def getDefaultJobParams() {
     ]
 }
 
+def getJobParams(String jobName, String jobFolder, String jenkinsfileName, String jobDescription = '') {
+    def jobParams = getDefaultJobParams()
+    jobParams.job.name = jobName
+    jobParams.job.folder = jobFolder
+    jobParams.jenkinsfile = jenkinsfileName
+    if (jobDescription) {
+        jobParams.job.description = jobDescription
+    }
+    return jobParams
+}
+
+def nightlyBranchFolder = "${KogitoConstants.KOGITO_DSL_NIGHTLY_FOLDER}/${JOB_BRANCH_FOLDER}"
+
 if (isMainBranch()) {
     // PR job is disabled for now as handled by another Jenkins
     // folder(KogitoConstants.KOGITO_DSL_PULLREQUEST_FOLDER)
 
     // setupPrJob(KogitoConstants.KOGITO_DSL_PULLREQUEST_FOLDER)
 }
+
+folder(KogitoConstants.KOGITO_DSL_NIGHTLY_FOLDER)
+folder(nightlyBranchFolder)
+setupSyncJob(nightlyBranchFolder, KogitoJobType.NIGHTLY)
 
 /////////////////////////////////////////////////////////////////
 // Methods
@@ -37,4 +54,34 @@ void setupPrJob(String jobFolder) {
     def jobParams = getDefaultJobParams()
     jobParams.job.folder = jobFolder
     KogitoJobTemplate.createPRJob(this, jobParams)
+}
+
+
+void setupSyncJob(String jobFolder, KogitoJobType jobType) {
+    def jobParams = getJobParams('rhpam-kogito-operator-sync', jobFolder, 'Jenkinsfile.upstream-operator-sync', 'RHPAM Kogito Operator synchronizing with Kogito operator')
+    jobParams.triggers = [ cron : '@midnight' ]
+    KogitoJobTemplate.createPipelineJob(this, jobParams).with {
+        parameters {
+            stringParam('DISPLAY_NAME', '', 'Setup a specific build display name')
+
+            stringParam('BUILD_BRANCH_NAME', "${GIT_BRANCH}", 'Set the Git branch to checkout')
+
+            // Build&Test information
+            stringParam('BDD_TEST_TAGS', '@rhpam', 'Execute only a subset of BDD tests')
+        }
+
+        environmentVariables {
+            env('JENKINS_EMAIL_CREDS_ID', "${JENKINS_EMAIL_CREDS_ID}")
+
+            env('REPO_NAME', 'rhpam-kogito-operator')
+            env('OPERATOR_IMAGE_NAME', 'rhpam-kogito-operator')
+            env('CONTAINER_ENGINE', 'podman')
+            env('CONTAINER_TLS_OPTIONS', '--tls-verify=false')
+            env('OPENSHIFT_API_KEY', 'OPENSHIFT_API')
+            env('OPENSHIFT_CREDS_KEY', 'OPENSHIFT_CREDS')
+            env('OPENSHIFT_REGISTRY_KEY', 'OPENSHIFT_REGISTRY')
+
+            env('GIT_AUTHOR', "${GIT_AUTHOR_NAME}")
+        }
+    }
 }
