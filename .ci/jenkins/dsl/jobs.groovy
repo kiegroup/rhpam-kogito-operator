3f8ae3ffdc6b3a4f5f6036b7ea3d739e1124a626
Tristan Radisson <tristan.radisson@gmail.com>
2021-09-21 16:50:39 +0200

    KOGITO-5730 Update DSL folder structure (#68) (#80)

--- .ci/jenkins/dsl/jobs.groovy
+++ .ci/jenkins/dsl/jobs.groovy
@@ -1,26 +1,11 @@
 import org.kie.jenkins.jobdsl.templates.KogitoJobTemplate
-import org.kie.jenkins.jobdsl.KogitoConstants
+import org.kie.jenkins.jobdsl.FolderUtils
 import org.kie.jenkins.jobdsl.Utils
 
 JENKINSFILE_PATH = '.ci/jenkins'
 
-boolean isMainBranch() {
-    return "${GIT_BRANCH}" == "${GIT_MAIN_BRANCH}"
-}
-
 def getDefaultJobParams() {
-    return [
-        job: [
-            name: 'rhpam-kogito-operator'
-        ],
-        git: [
-            author: "${GIT_AUTHOR_NAME}",
-            branch: "${GIT_BRANCH}",
-            repository: 'rhpam-kogito-operator',
-            credentials: "${GIT_AUTHOR_CREDENTIALS_ID}",
-            token_credentials: "${GIT_AUTHOR_TOKEN_CREDENTIALS_ID}"
-        ]
-    ]
+    return KogitoJobTemplate.getDefaultJobParams(this, 'rhpam-kogito-operator')
 }
 
 def getJobParams(String jobName, String jobFolder, String jenkinsfileName, String jobDescription = '') {
@@ -34,32 +19,16 @@ def getJobParams(String jobName, String jobFolder, String jenkinsfileName, Strin
     return jobParams
 }
 
-def nightlyBranchFolder = "${KogitoConstants.KOGITO_DSL_NIGHTLY_FOLDER}/${JOB_BRANCH_FOLDER}"
+setupSyncJob()
 
-// if (isMainBranch()) {
-    // PR job is disabled for now as handled by another Jenkins
-    // folder(KogitoConstants.KOGITO_DSL_PULLREQUEST_FOLDER)
-
-    // setupPrJob(KogitoConstants.KOGITO_DSL_PULLREQUEST_FOLDER)
-// }
-
-setupSyncJob(nightlyBranchFolder)
-
-setupProdUpdateVersionJob("${KogitoConstants.KOGITO_DSL_TOOLS_FOLDER}/${JOB_BRANCH_FOLDER}")
+setupProdUpdateVersionJob()
 
 /////////////////////////////////////////////////////////////////
 // Methods
 /////////////////////////////////////////////////////////////////
 
-void setupPrJob(String jobFolder) {
-    def jobParams = getDefaultJobParams()
-    jobParams.job.folder = jobFolder
-    KogitoJobTemplate.createPRJob(this, jobParams)
-}
-
-
-void setupSyncJob(String jobFolder) {
-    def jobParams = getJobParams('rhpam-kogito-operator-sync', jobFolder, "${JENKINSFILE_PATH}/Jenkinsfile.upstream-operator-sync", 'RHPAM Kogito Operator synchronizing with Kogito operator')
+void setupSyncJob() {
+    def jobParams = getJobParams('rhpam-kogito-operator-sync', FolderUtils.getNightlyFolder(this), "${JENKINSFILE_PATH}/Jenkinsfile.upstream-operator-sync", 'RHPAM Kogito Operator synchronizing with Kogito operator')
     jobParams.triggers = [ cron : '@midnight' ]
     KogitoJobTemplate.createPipelineJob(this, jobParams).with {
         parameters {
@@ -88,7 +57,7 @@ void setupSyncJob(String jobFolder) {
 }
 
 void setupProdUpdateVersionJob(String jobFolder) {
-    KogitoJobTemplate.createPipelineJob(this, getJobParams('rhpam-kogito-operator-update-prod-version', jobFolder, "${JENKINSFILE_PATH}/Jenkinsfile.update-prod-version", 'Update prod version for RHPAM Kogito Operator')).with {
+    KogitoJobTemplate.createPipelineJob(this, getJobParams('rhpam-kogito-operator-update-prod-version', FolderUtils.getToolsFolder(this), "${JENKINSFILE_PATH}/Jenkinsfile.update-prod-version", 'Update prod version for RHPAM Kogito Operator')).with {
         parameters {
             stringParam('JIRA_NUMBER', '', 'KIECLOUD-XXX or RHPAM-YYYY or else. This will be added to the commit and PR.')
             
